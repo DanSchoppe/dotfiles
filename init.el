b92b9ef06f0d2dd4dc059f4493859bea36838cb6
Dan Schoppe <dan.schoppe@gmail.com>
2015-04-14 15:38:57 -0500

    Updated clang-related tools.

--- init.el
+++ init.el
@@ -7,6 +7,29 @@
 ;; Text size
 (set-face-attribute 'default nil :height 95) ;; value in 1/10pt, so 100 will be 10pt font
 
+;; Word navigation for camelCase
+(global-subword-mode 1)
+
+;; Packages
+(package-initialize)
+(setq package-enable-at-startup nil)
+(setq auto-installed-packages
+      '(ace-jump-mode
+	autopair
+	color-theme
+	company
+	company-irony
+	irony-mode
+	exec-path-from-shell
+	flycheck
+	magit
+	multiple-cursors
+	projectile
+	))
+
+;; Set PATH
+(exec-path-from-shell-initialize)
+
 ;; Melpa
 (require 'package)
 (add-to-list 'package-archives
@@ -40,9 +63,6 @@
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
-;; Flycheck (error checking)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
 ;; Projectile (project definition)
 (projectile-global-mode)
 (setq projectile-enable-caching t)
@@ -61,11 +81,48 @@
 (autopair-global-mode 1)
 (setq autopair-autowrap t)
 
+;; Magit (git)
+(require 'magit)
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+;; RTags
+(require 'rtags)
+(global-set-key [C-return] 'rtags-find-symbol-at-point)
+
+;; Flycheck
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;;;; (setq c++-mode-hook nil)
+;;(add-hook 'c++-mode-hook
+;;          (lambda ()
+;;            (setq c-basic-offset 2)
+;;            (c-set-offset 'innamespace 0)
+;;            (setq tab-width 2)
+;;            (setq indent-tabs-mode nil)
+;;            (setq flycheck-check-syntax-automatically
+;;                  '(mode-enabled new-line save))
+;;            (setq flycheck-clang-language-standard "c++1y")
+;;            (setq flycheck-clang-standard-library "libc++")
+;;            (setq flycheck-clang-include-path
+;;                  '("/usr/lib/c++/v1"
+;;                    "/Users/seanbillig/local/include"
+;;                    "/Users/seanbillig/local/include/libxml2"
+;;                    "/Users/seanbillig/code/modeler_new/external"
+;;                    "/Users/seanbillig/code/core/dex/external"
+;;                    "/Users/seanbillig/code/core/newparser/external"
+;;                    "/Users/seanbillig/code/core/dex_newparser/src"
+;;                    "/Users/seanbillig/code/core/dex_newparser/external"
+;;                    "/Users/seanbillig/code/core/dex_newparser/build/debug"
+;;                    "/Users/seanbillig/code/core/dex/OSX/deps"
+;;                    "/Users/seanbillig/code/core/dex/build/debug"
+;;                    "/Users/seanbillig/code/core/dex/build/msgpack/include"
+;;                    "/Users/seanbillig/code/core/spirit_x3/include"))))
+
 ;; irony-mode smart auto-completion
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
-
 ;; replace the `completion-at-point' and `complete-symbol' bindings in
 ;; irony-mode's buffers by irony-mode's function
 (defun my-irony-mode-hook ()
@@ -75,52 +132,18 @@
     'irony-completion-at-point-async))
 (add-hook 'irony-mode-hook 'my-irony-mode-hook)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(global-set-key (kbd "C-SPC") 'company-complete)
 
+;; company-irony completion backend for irony-mode
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-irony))
-
 ;; (optional) adds CC special commands to `company-begin-commands' in order to
 ;; trigger completion at interesting places, such as after scope operator
 ;;     std::|
 (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+
+;; Company mode
 (add-hook 'after-init-hook 'global-company-mode)
-
-;;;; Yasnippet and Autocomplete
-;;(require 'yasnippet)
-;;(yas-global-mode 1)
-;;(require 'auto-complete-config)
-;;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-;;
-;;(ac-config-default)
-;;;;; set the trigger key so that it can work together with yasnippet on tab key,
-;;;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
-;;;;; activate, otherwise, auto-complete will
-;;(ac-set-trigger-key "TAB")
-;;(ac-set-trigger-key "<tab>")
-;;
-;;(require 'auto-complete-clang)
-;;;;(global-set-key (kbd "C-`") 'ac-complete-clang)
-;;;;(setq ac-auto-start nil)
-;;;;(setq ac-quick-help-delay 0.2)
-;;(define-key ac-mode-map  [(control tab)] 'auto-complete)
-;;(defun my-ac-config ()
-;;  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-;;  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-;;  ;; (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-;;  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
-;;  (add-hook 'css-mode-hook 'ac-css-mode-setup)
-;;  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-;;  (global-auto-complete-mode t))
-;;(defun my-ac-cc-mode-setup ()
-;;  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
-;;(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
-;;;; ac-source-gtags
-;;(my-ac-config)
-
-;; CEDET (Collection of Emacs Development Environment Tools)
-;;(load-file "~/Applications/cedet-1.1/common/cedet.el")
-;;(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
-;;(global-srecode-minor-mode 1)            ; Enable template insertion menu
 
 ;; Buffer-move
 (require 'buffer-move)

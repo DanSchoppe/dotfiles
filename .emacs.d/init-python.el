f749d2cc04f33c4719ba7f299e059f2d5075012a
Dan Schoppe <dan.schoppe@gmail.com>
2016-12-27 11:02:53 -0600

    [un]commenting python regions

--- .emacs.d/init-python.el
+++ .emacs.d/init-python.el
@@ -4,3 +4,9 @@
 	    (setq flycheck-check-syntax-automatically
 		  '(mode-enabled new-line save idle-change))
 	    (setq flycheck-idle-change-delay 0.1)))
+
+;; Commenting and uncommenting region
+(eval-after-load 'python-mode
+  #'(define-key python-mode-map (kbd "C-c C-c") nil))
+(global-set-key (kbd "C-c C-c") 'comment-region)
+(global-set-key (kbd "C-c C-u") 'uncomment-region)

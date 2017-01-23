e5db975fcb85b0b4c0b3510172aa5083beed52f3
Dan Schoppe <dan.schoppe@gmail.com>
2017-01-23 16:26:31 -0600

    zsh in emacs shell

--- .emacs.d/init-sh.el
+++ .emacs.d/init-sh.el
@@ -5,3 +5,11 @@
             (setq sh-basic-offset 2)
             (setq sh-indentation 2)
             ))
+
+;; shell inside of emacs
+(add-hook 'shell-mode-hook
+          (lambda ()
+            'ansi-color-for-comint-mode-on
+            (setq)))
+(add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
+(setq multi-term-program "/Users/danschoppe/local/bin/zsh")

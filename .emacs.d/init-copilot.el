4fda5aa0b355460e793746edd0243fec7451f2b1
Dan Schoppe <dan.schoppe@gmail.com>
2023-08-28 22:54:09 -0500

    Add emacs init-copilot.el

---
+++ .emacs.d/init-copilot.el
@@ -0,0 +1,12 @@
+(require 'quelpa-use-package)
+
+(use-package copilot
+  :quelpa (copilot :fetcher github
+                   :repo "zerolfx/copilot.el"
+                   :branch "main"
+                   :files ("dist" "*.el")))
+(add-hook 'prog-mode-hook 'copilot-mode)
+
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "M-p") 'copilot-previous-completion)
+(define-key copilot-completion-map (kbd "M-n") 'copilot-next-completion)

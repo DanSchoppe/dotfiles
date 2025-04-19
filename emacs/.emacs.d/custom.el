8c1cee746c57979f2a81c303ee197020048d32dc
Dan <dan.schoppe@gmail.com>
2025-04-19 01:31:33 -0500

    Fresh emacs config start: init.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,9 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7fea145741b3ca719ae45e6533ad1f49b2a43bf199d9afaee5b6135fd9e6f9b8"
+     default))
  '(js-switch-indent-offset 2)
  '(package-selected-packages
-   '(exec-path-from-shell company-terraform editorconfig quelpa-use-package use-package quelpa yasnippet zenburn-theme lsp-ui shackle solarized-theme ag atomic-chrome buffer-move color-theme cmake-mode dockerfile-mode git-gutter git-gutter-fringe helm-ag magit markdown-mode markdown-preview-mode multiple-cursors restclient terraform-mode undo-tree yaml-mode avy company flycheck helm-lsp helm-xref json-mode lsp-mode lsp-treemacs projectile which-key))
+   '(buffer-move exec-path-from-shell git-gutter helm multiple-cursors
+                 solarized-theme))
  '(undo-tree-auto-save-history nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,32 +1,129 @@
-;;; Dan Schoppe's Emacs init / configuration file
+(require 'package)
 
-(package-initialize)
-
-;; Melpa
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
 
 (setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-(defun load-init-file (file)
-	(load (locate-user-emacs-file file)))
-(add-to-list 'load-path "~/.emacs.d/external")
+;; Color theme
+(load-theme 'solarized-dark t)
 
-;; Load initializations
-(load-init-file "init-alias")
-(load-init-file "init-behavior")
-(load-init-file "init-calc")
-(load-init-file "init-funcs")
-(load-init-file "init-git")
-(load-init-file "init-gui")
-(load-init-file "init-org")
-(load-init-file "init-markdown")
-(load-init-file "init-copilot")
+;; Make PATH available:
+(exec-path-from-shell-initialize)
 
-;; ;; Languages
-;; (load-init-file "init-c")
-;; (load-init-file "init-python")
-;; (load-init-file "init-ruby")
-;; (load-init-file "init-sh")
-;; (load-init-file "init-sql")
-(load-init-file "init-web")
+;; save sessions between restarts
+(desktop-save-mode 1)
+
+;; Automatically revert buffers when the file changes on disk
+(global-auto-revert-mode t)
+
+;; Prevent backup file creation:
+(setq make-backup-files nil)
+
+;; Tabs and Whitespace
+(setq-default indent-tabs-mode nil
+              tab-width 2
+              require-final-newline t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Enable window layout undo / redo:
+(winner-mode 1)
+
+;; Turn javascript-mode on for *scratch*:
+(with-current-buffer "*scratch*" (javascript-mode))
+(setq initial-scratch-message nil)
+
+;; Disable wrapped lines
+(set-default 'truncate-lines t)
+
+;; Show line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;; Enable electric-pair-mode for auto-pairing
+(electric-pair-mode 1)
+
+;; Show matching parentheses
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+
+;; Word navigation for camelCase
+(global-subword-mode 1)
+
+;; Multiple cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c |") 'mc/vertical-align-with-space)
+
+;; Keybindings
+(global-set-key (kbd "C-x a") 'mark-whole-buffer) ;; select-all
+(global-set-key (kbd "M-n") (lambda () (interactive) (scroll-up-command 4)))
+(global-set-key (kbd "M-p") (lambda () (interactive) (scroll-down-command 4)))
+
+;; Buffer switching
+(global-set-key (kbd "C-x C-p") 'previous-buffer)
+(global-set-key (kbd "C-x C-n") 'next-buffer)
+
+;; Buffer-move
+(require 'buffer-move)
+(global-set-key (kbd "C-M-i") 'buf-move-up)
+(global-set-key (kbd "C-M-k")  'buf-move-down)
+(global-set-key (kbd "C-M-j")  'buf-move-left)
+(global-set-key (kbd "C-M-l")  'buf-move-right)
+
+;; Window navigation
+(global-set-key (kbd "M-j") 'windmove-left)
+(global-set-key (kbd "M-l") 'windmove-right)
+(global-set-key (kbd "M-i") 'windmove-up)
+(global-set-key (kbd "M-k") 'windmove-down)
+
+;; Window resizing
+(global-set-key (kbd "M-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "M-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "M-<down>") 'shrink-window)
+(global-set-key (kbd "M-<up>") 'enlarge-window)
+
+;; Comment / uncomment regions:
+(global-set-key (kbd "C-c C-c") 'comment-region)
+(global-set-key (kbd "C-c C-u") 'uncomment-region)
+
+
+;; Use ibuffer for listing open buffers
+(defalias 'list-buffers 'ibuffer)
+
+;; Show git line symbols
+(global-git-gutter-mode 1)
+
+;; Helm completion
+(require 'helm)
+(setq helm-display-buffer-default-height 0.3)
+(setq helm-echo-input-in-header-line t)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(setq helm-M-x-fuzzy-match t)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-M-s") 'helm-occur)
+
+;; Avoid performance issues in files with very long lines.
+(global-so-long-mode 1)
+
+;; GUI-related Customizations
+(when (display-graphic-p)
+  ;; Set font size
+  (set-face-attribute 'default nil :height 130)
+
+  ;; Disable startup screen and toolbar
+  (setq inhibit-startup-screen t)
+  (tool-bar-mode -1)
+
+  ;; Maximize window on startup
+  (add-to-list 'default-frame-alist '(fullscreen . maximized))
+)
+
+(provide 'init)
